573752c8b6b578a814572486d75847f7419575d6
sunjiafu <84288682@qq.com>
2022-11-03 00:51:59 +0800

    setCurrentUrl

--- app/Admin/Forms/SystemSetting.php
+++ app/Admin/Forms/SystemSetting.php
@@ -16,12 +16,12 @@ class SystemSetting extends Form
      */
     public function handle(array $input)
     {
-        Cache::put('system-setting',$input);
+        Cache::put('system-setting', $input);
 
         return $this
-				->response()
-				->success('Processed successfully.')
-				->refresh();
+            ->response()
+            ->success('Processed successfully.')
+            ->refresh();
     }
 
     /**
@@ -29,30 +29,31 @@ class SystemSetting extends Form
      */
     public function form()
     {
-        $this->tab('系统设置', function () {
+        $this->tab(
+            '系统设置',
+            function () {
 
-            $this->text('title','网站名称')->required();
+                $this->text('title', '网站名称')->required();
+            }
 
-        }
-    
-    );
+        );
 
-    $this->tab('TelggramBot', function () {
+        $this->tab(
+            'TelggramBot',
+            function () {
 
-        $this->markdown('support','服务支持回复内容')->required();
+                $this->markdown('support', '服务支持回复内容')->required();
 
-        $this->markdown('reviews','评论回复内容')->required();
+                $this->markdown('reviews', '评论回复内容')->required();
 
-        $this->markdown('allgiftcard','所有礼品卡回复内容')->required();
+                $this->markdown('allgiftcard', '所有礼品卡回复内容')->required();
 
+                
+            }
 
+            
 
-
-    }
-
-);
-
-
+        )->action('https://t.me');
     }
 
     /**
@@ -63,6 +64,5 @@ class SystemSetting extends Form
     public function default()
     {
         return Cache::get('system-setting');
-
     }
 }
