e75634100c7e3c6904f3e817840c31fc8c49ba78
sunjiafu <84288682@qq.com>
2022-11-02 18:46:45 +0800

    update mywebhook

--- app/Telegram/Handlers/MyWebhookHandler.php
+++ app/Telegram/Handlers/MyWebhookHandler.php
@@ -357,9 +357,9 @@ class MyWebhookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
                     }
 
                     $this->chat->markdown(
-                        'OrderId:'.$order->order_sn.
-                        'Product:'.$order->title.
-                        'Price:'.$order->price.
+                        'OrderId:'.$order->order_sn.'\n'.
+                        'Product:'.$order->title.'\n'.
+                        'Price:'.$order->price.'\n'.
                         'Code:'.$order->code)
                         
                         ->send();
