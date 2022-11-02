1c68c1b0882621cbc38aa6ba8ca0712e46dc553e
sunjiafu <84288682@qq.com>
2022-11-02 12:38:36 +0800

    update keyboard

--- app/Telegram/Handlers/MyWebhookHandler.php
+++ app/Telegram/Handlers/MyWebhookHandler.php
@@ -332,19 +332,10 @@ class MyWebhookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
                 break;
             case '🛒All Giftcard':
                 break;
-                case '🛎️Support':
-                    $this->chat->markdown("
-                    *24/7 Customer Service Support*
-                    We guarantee the provision of services at the highest level, availability 24 hours a day, 7 days a week, 365 days a year. You will enjoy your purchase and we are sure that you will return to us again.
-                    *How to contact us*
-                    👉WhatsApp:24/7[ Support Giftcards](https://wa.me/31623518851 '24/7 Support Giftcards')
-                    👉Telegram:@DiscountGiftcard_support
-                    👉Auto-shop - https://bit.ly/3BLj9fb
-                    *We CAN help with the following*
-                    - Help placing an order
-                    - Questions about an existing order
-                    - Questions about using this Bot ")->send();
-                    break;
+            case '🛎️Support':
+                $this->chat->markdown("
+                    Reviews")->send();
+                break;
         }
     }
 }
