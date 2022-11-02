8533d49866ead9c1cf1d51b5c9ec009559205a00
sunjiafu <84288682@qq.com>
2022-11-02 13:00:53 +0800

    update keyboard

--- app/Telegram/Handlers/MyWebhookHandler.php
+++ app/Telegram/Handlers/MyWebhookHandler.php
@@ -335,7 +335,8 @@ class MyWebhookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
             case '☎️Support':
                 $this->chat->markdown("
  *24/7 Customer Service Support*\n\n We guarantee the provision of services at the highest level, availability 24 hours a day, 7 days a week, 365 days a year. You will enjoy your purchase and we are sure that you will return to us again.\n*How to contact us*\n
- 👉WhatsApp:[24/7Support Giftcards](https://wa.me/31623518851)
+ 👉WhatsApp:[24/7Support Giftcards](https://wa.me/31623518851),
+ 👉Telegram:@DiscountGiftcard_support
  
 
  *We CAN help with the following*
